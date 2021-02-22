72ec1d23b1b29d7326316e85fffadc74c9b47824
koushith <koushith97@gmail.com>
2021-02-22 17:52:11 +0530

    fixes css in issue in signup page

--- components/auth/SignUpWithEmail.js
+++ components/auth/SignUpWithEmail.js
@@ -6,41 +6,32 @@ import { SignUpWrapper } from './Signup.styles';
 function SignUpWithEmail({ setWithEmail }) {
   return (
     <SignUpWrapper>
+      <Input
+        icon={<Icons.Mail />}
+        placeholder='Enter your Email'
+        size='large'
+      />
       <Spacer y={0.5} />
-      <div className='login-form'>
-        <Text as h2>
-          Sign Up to Infrastructure
-        </Text>
+      <Input
+        icon={<Icons.Lock />}
+        placeholder='Enter your Password'
+        type='password'
+        size='large'
+      />
+      <Spacer y={0.5} />
+      <Input.Password placeholder='Re-enter your password' size='large' />
+      <Spacer y={0.5} />
+      <Button type='secondary' size='large' className='login-form__btn'>
+        Sign Up
+      </Button>
+      <Spacer y={0.5} />
 
-        <div className='login-form__actions'>
-          <Input
-            icon={<Icons.Mail />}
-            placeholder='Enter your Email'
-            size='large'
-          />
-          <Spacer y={0.5} />
-          <Input
-            icon={<Icons.Lock />}
-            placeholder='Enter your Password'
-            type='password'
-            size='large'
-          />
-          <Spacer y={0.5} />
-          <Input.Password placeholder='Re-enter your password' size='large' />
-          <Spacer y={0.5} />
-          <Button type='secondary' size='large' className='login-form__btn'>
-            Sign Up
-          </Button>
-          <Spacer y={0.5} />
-
-          <Text as p onClick={() => setWithEmail(true)}>
-            {' '}
-            You can also use{' '}
-            <span className='signup-option'>GitHub, GitLab or Google</span> to
-            Signup
-          </Text>
-        </div>
-      </div>
+      <Text as p onClick={() => setWithEmail(true)}>
+        {' '}
+        You can also use{' '}
+        <span className='signup-option'>GitHub, GitLab or Google</span> to
+        Signup
+      </Text>
 
       <Spacer y={0.5} />
     </SignUpWrapper>
